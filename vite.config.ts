340f9027fbf64201c80e4f447eeaba8f37430286
Birhat Krd <birhatakrd@gmail.com>
2025-12-22 19:40:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,12 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    // ئەڤە کلیلێ ژ Netlify دخوینیت
-    const env = loadEnv(mode, process.cwd(), '');
-    
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        // ئەڤە کلیلێ دگەهینیتە ناڤ کودی ب ناڤێ API_KEY
-        'process.env.API_KEY': JSON.stringify(env.API_KEY || env.GEMINI_API_KEY),
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
